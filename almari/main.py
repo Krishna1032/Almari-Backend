29ea5cc7e54c5afc17f714ea013a6af60f3986d6
Krishna Lamsal <zerokl7600@gmail.com>
2022-09-28 12:15:35 +0545

    Added Middleware

--- almari/main.py
+++ almari/main.py
@@ -2,8 +2,17 @@ from fastapi import FastAPI
 from .database import engine
 from .import models
 from .routers import login, users, posts
-
-models.Base.metadata.create_all(bind=engine)
+from fastapi.middleware.cors import CORSMiddleware
+origins =["*"]
+# models.Base.metadata.create_all(bind=engine)
+app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app =  FastAPI()
 
